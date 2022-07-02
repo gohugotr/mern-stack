fbb28ae9b3ac46a085ca2efa6dbea0b99d68ff80
Cengiz YILMAZ <terabilgi@gmail.com>
2022-07-03 02:27:42 +0300

    Get, post, put ve delete api route leri tanımlandı

--- backend/routes/notRoute.js
+++ backend/routes/notRoute.js
@@ -3,7 +3,19 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.status(200).json({ mesaj: 'get notlar' })
+  res.status(200).json({ mesaj: 'Get notlar' })
+})
+
+router.post('/',(req,res) =>{
+  res.status(200).json({Mesaj: 'Post Notlar'})
+})
+
+router.put('/:id',(request,response) => {
+  response.status(200).json({Mesaj: `put metodu:  ${request.params.id} nolu id güncellendi`})
+})
+
+router.delete('/:id',(request, response) => {
+  response.status(200).json({Mesaj: `DELETE Metodu: ${request.params.id} nolu id silindi`})
 })
 
 export default router

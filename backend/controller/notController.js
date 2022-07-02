20a8dcfbea06670c503969db0569e7f816f045dd
Cengiz YILMAZ <terabilgi@gmail.com>
2022-07-03 02:53:29 +0300

    Controller yazılarak router sadeleştirildi

---
+++ backend/controller/notController.js
@@ -0,0 +1,15 @@
+export const getNotlar = (request, response) => {
+  response.status(200).json({ Mesaj: `Controller GET Notlar` })
+}
+
+export const setNotlar = (request, response) => {
+  response.status(200).json({ Mesaj: `Controller POST Notlar` })
+}
+
+export const putNotlar = (req, res) => {
+  res.status(200).json({ Mesaj: `Controller PUT Notlar ${req.params.id} Nolu ID` })
+}
+
+export const deleteNotlar = (req, res) => {
+  res.status(200).json({ Mesaj: `Controller DELETE Notlar ${req.params.id} Nolu ID` })
+}
--- backend/routes/notRoute.js
+++ backend/routes/notRoute.js
@@ -2,20 +2,27 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.status(200).json({ mesaj: 'Get notlar' })
-})
+// router.get('/', (req, res) => {
+//   res.status(200).json({ mesaj: 'Get notlar' })
+// })
 
-router.post('/',(req,res) =>{
-  res.status(200).json({Mesaj: 'Post Notlar'})
-})
+// router.post('/',(req,res) =>{
+//   res.status(200).json({Mesaj: 'Post Notlar'})
+// })
 
-router.put('/:id',(request,response) => {
-  response.status(200).json({Mesaj: `put metodu:  ${request.params.id} nolu id güncellendi`})
-})
+// router.put('/:id',(request,response) => {
+//   response.status(200).json({Mesaj: `put metodu:  ${request.params.id} nolu id güncellendi`})
+// })
 
-router.delete('/:id',(request, response) => {
-  response.status(200).json({Mesaj: `DELETE Metodu: ${request.params.id} nolu id silindi`})
-})
+// router.delete('/:id',(request, response) => {
+//   response.status(200).json({Mesaj: `DELETE Metodu: ${request.params.id} nolu id silindi`})
+// })
+
+import {getNotlar, setNotlar, putNotlar, deleteNotlar} from '../controller/notController.js'
+
+router.get('/', getNotlar)
+router.post('/', setNotlar)
+router.put('/:id', putNotlar)
+router.delete('/:id', deleteNotlar)
 
 export default router
